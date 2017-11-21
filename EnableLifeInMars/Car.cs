aeadd8ea5b3f7ed1045513e37c18583aaf02f0f5
moonlaughs <iza8848@gmail.com>
2017-11-21 11:40:57 +0100

    Update Car.cs
    commit from Git

--- EnableLifeInMars/Car.cs
+++ EnableLifeInMars/Car.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +12,7 @@ namespace EnableLifeInMars
         private string Name;
         public Car()
         {
-            
+            //ctormvdkfmvkm
         }
     }
 }
